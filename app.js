764e8e6469739dd96b31bfc767a11e2536e9f19c
rakrisi <radhesingh12321@gmail.com>
2021-08-11 11:14:49 +0530

    update1.2

--- app.js
+++ app.js
@@ -3,35 +3,70 @@ const app = Vue.createApp({
     return {
       question: '',
       timer: '',
+      starts:'Start',
       q:[],
-      t:[]
+      t:[],
+      timestamp:'',
+      isActive: true,
+      istimer: false,
+      errors:[]
     };
+    
   },
+  created() {
+    setInterval(this.cr, 1000);
+},
   methods: {
     addgoal()
     {
+      if (!this.question) {
+        this.errors.push('Question required.');
+      }
+    else if (!this.timer) {
+        this.errors.push('Timer required.');
+      }
+      else{
       this.q.push(this.question);
       this.t.push(this.timer);
-      console.log(" q length" + this.q.length);
-      console.log(" t length" + this.t.length);
+      this.isActive=false;
+      
+      }
+      // console.log(" q length" + this.q.length);
+      // console.log(" t length" + this.t.length);
       //this.start();
     },
+    cr(){
+ 
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        const dateTime =  time;
+        this.timestamp = dateTime;
+
+    },
     start() {
-     
+     this.starts = ' Exam Started....';
+     this.istimer= true;
       for( let i=0; i< this.q.length; i++)
       {
+        
        // console.log("timer"+this.q[i]);
         for(let j=0;j<this.q[i];j++)
         {
+          if(j ==this.q[i] )
+        {
+          this.starts = ' Exam End';
+          this.play();
+        }
           setTimeout(() => {
                 this.play(),
                 console.log("start");
+                
               //console.log(j);
               //console.log("timer"+this.t[i]);
               //console.log("question"+this.q[i]);
               //}, j * this.t[i] *60* 1000);
             }, j*this.t[i]*60*1000);
-              
+             
         }
 
       }
